2d758da95bb622262563030f91951e7a0b5831d9
rodrigo <rodrigo@gato>
2016-07-04 18:18:51 -0400

    Minor refactor

--- src/grasping_lib/GraspingUtils.hpp
+++ src/grasping_lib/GraspingUtils.hpp
@@ -15,9 +15,6 @@
 #include <opencv2/ml/ml.hpp>
 #include "Metric.hpp"
 
-// SVM's shared pointer
-//typedef boost::shared_ptr<CvSVM> CvSVMPtr;
-
 // Class implementing several utilities for the grasping node's routines
 class GraspingUtils
 {
@@ -73,63 +70,6 @@ public:
 
 		return filteredCloud;
 	}
-
-	// Generates a permutation of the given matrix
-	static inline void generatePermutation(const cv::Mat &matrix_, const int permutationSize_, const int permutationNumber_, cv::Mat &permutation_)
-	{
-		int begin = permutationNumber_ * permutationSize_;
-		int end = matrix_.cols;
-		matrix_.colRange(begin, end).copyTo(permutation_.colRange(0, end - begin));
-
-		begin = 0;
-		end = permutationNumber_ * permutationSize_;
-		if (end - begin > 0)
-			matrix_.colRange(begin, end).copyTo(permutation_.colRange(permutation_.cols - (end - begin), permutation_.cols));
-	}
-
-	// Prepares the clasificator for the labeling process
-//	static CvSVMPtr prepareClasificator(const cv::Mat &bow_, const std::map<std::string, std::string> &bowParams_)
-//	{
-//		std::vector<std::string> params;
-//		std::string metricParams = bowParams_.at("metric");
-//		boost::trim_if(metricParams, boost::is_any_of("[]"));
-//		boost::split(params, metricParams, boost::is_any_of("[,]"), boost::token_compress_on);
-//
-//		int permutationSize = bow_.cols;
-//		if (boost::iequals(params[0], metricType[METRIC_CLOSEST_PERMUTATION]) || boost::iequals(params[0], metricType[METRIC_CLOSEST_PERMUTATION_WITH_CONFIDENCE]))
-//			permutationSize = boost::lexical_cast<int>(params[1]);
-//
-//		int permutationNumber = bow_.cols / permutationSize;
-//
-//		// Prepare the training data
-//		cv::Mat trainingData = cv::Mat::zeros(bow_.rows * permutationNumber, bow_.cols, CV_32FC1);
-//		cv::Mat labels = cv::Mat::zeros(bow_.rows * permutationNumber, 1, CV_32FC1);
-//
-//		for (int i = 0; i < bow_.rows; i++)
-//		{
-//			cv::Mat currentCentroid = bow_.row(i);
-//			for (int j = 0; j < permutationNumber; j++)
-//			{
-//				int index = i * permutationNumber + j;
-//
-//				cv::Mat permutation = trainingData.row(index);
-//				generatePermutation(currentCentroid, permutationSize, j, permutation);
-//				labels.at<float>(index, 0) = (float) (i + 1);
-//			}
-//		}
-//
-//		// Set SVM parameters
-//		CvSVMParams svmParams;
-//		svmParams.svm_type = CvSVM::C_SVC;
-//		svmParams.kernel_type = CvSVM::LINEAR;
-//		svmParams.term_crit = cvTermCriteria(CV_TERMCRIT_ITER + CV_TERMCRIT_EPS, 100, 1e-6);
-//
-//		// Train the SVM
-//		CvSVMPtr svm = CvSVMPtr(new CvSVM());
-//		svm->train(trainingData, labels, cv::Mat(), cv::Mat(), svmParams);
-//
-//		return svm;
-//	}
 
 	// Downsamples the given cloud
 	static inline void downsampleCloud(const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud_, const float voxelSize_, pcl::PointCloud<pcl::PointXYZ>::Ptr &sampledCloud_)
