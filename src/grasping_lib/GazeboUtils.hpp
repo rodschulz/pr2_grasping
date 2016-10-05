80602ba9d14d58e409e8ce581363ec29998c6544
rodrigo <rodrigo@gato>
2016-10-05 12:15:50 -0300

    Implemented utilities for gazebo simulation

--- src/grasping_lib/GazeboUtils.hpp
+++ src/grasping_lib/GazeboUtils.hpp
@@ -6,9 +6,12 @@
 
 #include <utility>
 #include <string>
+#include <fstream>
+#include <streambuf>
 #include <ros/ros.h>
 #include <gazebo_msgs/GetWorldProperties.h>
 #include <gazebo_msgs/GetModelState.h>
+#include <gazebo_msgs/SetModelState.h>
 #include <gazebo_msgs/DeleteModel.h>
 #include <gazebo_msgs/SpawnModel.h>
 
@@ -35,14 +38,14 @@ public:
 
 
 	/**************************************************/
-	static inline bool getModelState(const std::string &modelName_, gazebo_msgs::GetModelState::Response &state_)
+	static inline bool getModelState(const std::string &modelName_, geometry_msgs::Pose &state_)
 	{
 		gazebo_msgs::GetModelState state;
 		state.request.model_name = modelName_;
 		if (ros::service::call("/gazebo/get_model_state", state))
 		{
 			if (state.response.success)
-				state_ = state.response;
+				state_ = state.response.pose;
 			return state.response.success;
 		}
 		return false;
@@ -50,7 +53,7 @@ public:
 
 
 	/**************************************************/
-	static inline bool getWorldState(std::map<std::string, gazebo_msgs::GetModelStateResponse> &state_, const std::map<std::string, bool> &ignore_ = std::map<std::string, bool>())
+	static inline bool getWorldState(std::map<std::string, geometry_msgs::Pose> &stateMap_, const std::map<std::string, bool> &ignore_ = std::map<std::string, bool>())
 	{
 		bool result = false;
 
@@ -59,12 +62,12 @@ public:
 		{
 			result = true;
 
-			state_.clear();
+			stateMap_.clear();
 			for (std::vector<std::string>::const_iterator it = models.begin(); it != models.end(); it++)
 			{
-				gazebo_msgs::GetModelState::Response modelState;
+				geometry_msgs::Pose modelState;
 				if (getModelState(*it, modelState))
-					state_[*it] = modelState;
+					stateMap_[*it] = modelState;
 				else
 				{
 					ROS_WARN("Cant get model state: %s", it->c_str());
@@ -80,6 +83,19 @@ public:
 
 
 	/**************************************************/
+	static inline bool setModelState(const std::string &modelName_, const geometry_msgs::Pose &pose_, const std::string &referenceFrame_)
+	{
+		gazebo_msgs::SetModelState state;
+		state.request.model_state.model_name = modelName_;
+		state.request.model_state.pose = pose_;
+		state.request.model_state.reference_frame = referenceFrame_;
+		if (ros::service::call("/gazebo/set_model_state", state))
+			return state.response.success;
+		return false;
+	}
+
+
+	/**************************************************/
 	static inline bool deleteModel(const std::string &modelName_)
 	{
 		gazebo_msgs::DeleteModel remove;
@@ -91,32 +107,38 @@ public:
 
 
 	/**************************************************/
-	static inline bool spawnModel(const std::string &modelName_, const std::string &modelXML_, const gazebo_msgs::GetModelState::Response &state_)
+	static inline bool spawnModel(const std::string &modelName_, const std::string &modelFileLocation_, const geometry_msgs::Pose &state_)
 	{
 		gazebo_msgs::SpawnModel spawn;
 		spawn.request.model_name = modelName_;
-		spawn.request.model_xml = modelXML_;
-		spawn.request.initial_pose = state_.pose;
+		spawn.request.model_xml = loadModelFile(modelFileLocation_);
+		spawn.request.robot_namespace = "";
+		spawn.request.initial_pose = state_;
+		spawn.request.reference_frame = "world";
 
 		if (ros::service::call("/gazebo/spawn_sdf_model", spawn))
+		{
+			ROS_INFO_STREAM(spawn.response.status_message);
 			return spawn.response.success;
-
-		return true;
+		}
+		return false;
 	}
 
-
-	// /**************************************************/
-	// static inline bool respawnModel(const std::string &modelName_, const std::string &modelXML_, const gazebo_msgs::GetModelState::Response &state_)
-	// {
-	// 	if (deleteModel(modelName_))
-	// 		if (spawnModel(modelName_, modelXML_, state_))
-	// 			return true;
-	// 	return false;
-	// }
 
 private:
 	// Constructor
 	GazeboUtils();
 	// Destructor
 	~GazeboUtils();
+
+
+	/**************************************************/
+	static inline std::string loadModelFile(const std::string &modelLocation_)
+	{
+		std::ifstream modelFile(modelLocation_.c_str(), std::ifstream::in);
+		std::string str;
+		str.assign((std::istreambuf_iterator<char>(modelFile)), std::istreambuf_iterator<char>());
+
+		return str;
+	}
 };
